2ba32b5494641b433dba83e7754afe4a370d74b1
Rupam Sutar <rupamsutar281998@gmail.com>
2023-02-28 08:24:10 +0530

    Reading and writing file asynchronously

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
-const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
+// const textOut = `This is what we know about the avocado: ${textIn}. \nCreated on ${Date.now()}`;
 
-fs.writeFileSync("./txt/output.txt", textOut);
+// fs.writeFileSync("./txt/output.txt", textOut);
+
+// Asynchronuous Code :
+
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+            fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, err => {
+                console.log(`Your file has been written`);
+            })
+        })
+    })
+})
+
+console.log("Data is running in background : ");
