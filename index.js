e1b3cd4857802e2497609c0281700d6ffb7a9f9b
Rupam Sutar <rupamsutar281998@gmail.com>
2023-02-28 08:47:30 +0530

    Server Started

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const http = require("http");
 
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
@@ -8,14 +9,24 @@ const fs = require("fs");
 
 // Asynchronuous Code :
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-    fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-        fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-            fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, err => {
-                console.log(`Your file has been written`);
-            })
-        })
-    })
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//     fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//         fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//             fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, err => {
+//                 console.log(`Your file has been written`);
+//             })
+//         })
+//     })
+// })
+
+// console.log("Data is running in background : ");
+
+/////////////////////////////////  Creating Server  ///////////////////////////////////////
+
+const server = http.createServer((req, res) => {
+    res.end("Hello from the server !");
 })
 
-console.log("Data is running in background : ");
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening on port 8000");
+})
