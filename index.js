fc0effc5d896806dbe60436ebf5571177f428a80
Rupam Sutar <rupamsutar281998@gmail.com>
2023-03-16 23:28:11 +0530

    Creating the server

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const fs = require("fs");
+const http = require("http");
 
+// fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
+//     fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
+//         fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//             fs.writeFile("./txt/final.txt", `${data2} \n\n${data3}`, (err) => {
+//                 console.log("File has been succesfully written");
+//             });
+//         });
+//     });
+// });
 
-fs.readFile("./txt/start.txt", "utf-8", (err, data) => {
-    fs.readFile(`./txt/${data}.txt`, "utf-8", (err, data2) => {
-        fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-            fs.writeFile("./txt/final.txt", `${data2} \n\n${data3}`, (err) => {
-                console.log("File has been succesfully written");
-            });
-        });
-    });
-});
+const server = http.createServer((req, res) => {
+    console.log(req)
+    res.end("Hello from the server");
+})
+
+server.listen(8000, "127.0.0.1", () => {
+    console.log("Listening to the server on port 8000");
+})
