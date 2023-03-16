6d90592b519459a801b526c695df2da9807489c1
Rupam Sutar <rupamsutar281998@gmail.com>
2023-03-16 21:39:18 +0530

    File written in nodejs

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ const fs = require("fs");
 const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 const textOut = `This is what we know about the avacado: \n ${textIn} \n \n Created on ${Date.now()}`;
-fs.writeFileSync("./txt/")
+fs.writeFileSync("./txt/output.txt", textOut);
+
